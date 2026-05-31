94be1732e89b33c2d3d64cf9c242fe0eb3b415dc
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-05-31 15:41:37 +0200

    Ejercicios de arrays

---
+++ 01-basics/arrays.js
@@ -0,0 +1,18 @@
+const videojuegos = [
+    "Minecraft",
+    "Red Dead Redemption 2",
+    "Stardew Valley"
+];
+console.log(videojuegos[10]);
+//Ejercicio 1
+console.log(videojuegos);
+console.log(videojuegos[0]);
+console.log(videojuegos[2]);
+
+//Ejercicio 2
+videojuegos.push("Terraria");
+console.log(videojuegos);
+
+//Ejercicio 3
+const niveles = [1, 2, 3, 4, 5];
+console.log(niveles.length);
